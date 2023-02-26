e78e29e7f23f94ac9d66cdb65c0711a7935c270a
Ahmed Raza <118352440+Ahmedscript@users.noreply.github.com>
2023-02-26 22:40:09 +0500

    Add files via upload
    html_test

---
+++ js/test.js
@@ -0,0 +1,216 @@
+var all_Marks = 0;
+function test() {
+    var subMit_btn = document.getElementById('submit');
+
+    // 1 //
+    var input_1 = document.getElementById('input_1').value;
+    // /1 //
+    // 2 //
+    var input_2 = document.getElementById('input_2').value;
+    // /2 //
+    // 3 //
+    var input_3 = document.getElementById('input_3').value;
+    // /3 //
+    // 4 //
+    var input_4 = document.getElementById('input_4').value;
+    // /4 //
+    // 5 //
+    var input_5 = document.getElementById('input_5').value;
+    // /5 //
+    // 6 //
+    var input_6 = document.getElementById('input_6').value;
+    // /6 //
+    // 7 //
+    var input_7 = document.getElementById('input_7').value;
+    // /7 //
+    // 8 //
+    var input_8 = document.getElementById('input_8').value;
+    // /8 //
+    // 9 //
+    var input_9 = document.getElementById('input_9').value;
+    // /9 //
+    // 10 //
+    var input_10 = document.getElementById('input_10').value;
+    // /10 //
+
+    ///  ANSWERA  ///
+    var ANSWERA_1 = "HyperText Markup Language";
+    var ANSWERA_2 = "HTML, Head, Title, Body";
+    var ANSWERA_3 = "<pre>";
+    var ANSWERA_4 = "<br>";
+    var ANSWERA_5 = `"<a href= "www.javatpoint.com"> javaTpoint.com </a>"`;
+    var ANSWERA_6 = "<ol>";
+    var ANSWERA_7 = ".html";
+    var ANSWERA_8 = "<Doctype HTML>";
+    var ANSWERA_9 = "HTML";
+    var ANSWERA_10 = "<h1> to <h6>";
+    ///  ANSWERA  ///
+    // 1 //
+    if (input_1 == ANSWERA_1) {
+        var span_1 = document.getElementById('span_1').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_1 == "") {
+        var span_1 = document.getElementById('span_1').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_1 = document.getElementById('span_1').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /1 //
+    // 2 //
+    if (input_2 == ANSWERA_2) {
+        var span_2 = document.getElementById('span_2').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_2 == "") {
+        var span_2 = document.getElementById('span_2').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_2 = document.getElementById('span_2').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /2 //
+    // 3 //
+    if (input_3 == ANSWERA_3) {
+        var span_3 = document.getElementById('span_3').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_3 == "") {
+        var span_3 = document.getElementById('span_3').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_3 = document.getElementById('span_3').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /3 //
+    // 4 //
+    if (input_4 == ANSWERA_4) {
+        var span_4 = document.getElementById('span_4').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_4 == "") {
+        var span_4 = document.getElementById('span_4').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_4 = document.getElementById('span_4').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /4 //
+    // 5 //
+    if (input_5 == ANSWERA_5) {
+        var span_5 = document.getElementById('span_5').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_5 == "") {
+        var span_5 = document.getElementById('span_5').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_5 = document.getElementById('span_5').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /5 //
+    // 6 //
+    if (input_6 == ANSWERA_6) {
+        var span_6 = document.getElementById('span_6').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_6 == "") {
+        var span_6 = document.getElementById('span_6').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_6 = document.getElementById('span_6').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /6 //
+    // 7 //
+    if (input_7 == ANSWERA_7) {
+        var span_7 = document.getElementById('span_7').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_7 == "") {
+        var span_7 = document.getElementById('span_7').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_7 = document.getElementById('span_7').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /7 //
+    // 8 //
+    if (input_8 == ANSWERA_8) {
+        var span_8 = document.getElementById('span_8').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_8 == "") {
+        var span_8 = document.getElementById('span_8').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_8 = document.getElementById('span_8').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /8 //
+    // 9 //
+    if (input_9 == ANSWERA_9) {
+        var span_9 = document.getElementById('span_9').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_9 == "") {
+        var span_9 = document.getElementById('span_9').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_9 = document.getElementById('span_9').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /9 //
+    // 10 //
+    if (input_10 == ANSWERA_10) {
+        var span_10 = document.getElementById('span_10').innerHTML = "";
+        all_Marks += 10;
+        subMit_btn.style.display = "none";
+    }
+    else if (input_10 == "") {
+        var span_10 = document.getElementById('span_10').innerHTML = "fill your field";
+        subMit_btn.style.display = "block";
+    }
+    else {
+        var span_10 = document.getElementById('span_10').innerHTML = "";
+        subMit_btn.style.display = "none";
+    }
+    // /10 //
+    return false;
+}
+function result() {
+    var main_father_div = document.getElementById('main_father_div').style.display = "none";
+    document.getElementById('submit').style.display = "none";
+    document.getElementById('result_div').style.display = "none";
+    document.getElementById('main_result_div').style.display = "block";
+    var risult_heading = document.getElementById('risult_heading');
+    var obtained_marks = all_Marks;
+    var total_marks = 100;
+    var percentage = all_Marks / total_marks * 100;
+    if (percentage < 50) {
+        risult_heading.innerHTML = "FAIL" + " " + percentage + "%";
+    }
+    else if (percentage > 50) {
+        risult_heading.innerHTML = "PASS" + " " + percentage + "%";
+    }
+    return false
+}
